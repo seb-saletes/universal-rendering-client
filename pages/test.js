20085d4b722ab42323de61d2e52432861500b7f4
Seb Saletes <seb.saletes@gmail.com>
2018-11-09 16:45:59 +0100

    test section gtmetrix

---
+++ pages/test.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export default () => (
+  <div>
+    <section>
+      <h1>Title 1 in section 1</h1>
+    </section>
+    <section>
+      <h1>Title 2 in section 2</h1>
+    </section>
+    <section>
+      <h1>Title 3 in section 3</h1>
+    </section>
+  </div>
+)
